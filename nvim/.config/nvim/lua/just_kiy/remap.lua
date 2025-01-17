5f7a5e0c1a5395248e3de11df4bbd821160a37da
Никита Ковалев <nikitakovalev@MacBook-Pro-nkovalev.local>
2025-01-17 12:11:55 +0300

    fix: nvim-tmux panes navigations

--- nvim/.config/nvim/lua/just_kiy/remap.lua
+++ nvim/.config/nvim/lua/just_kiy/remap.lua
@@ -3,3 +3,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")

5d7de694945386ac515864a726ba64422d8f776e
Никита Ковалев <nikitakovalev@MacBook-Pro-nkovalev.local>
2025-01-17 15:18:16 +0300

    nvim: updated gitsigns config

--- nvim/.config/nvim/lua/just_kiy/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/just_kiy/plugins/gitsigns.lua
@@ -1,7 +1,37 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require("gitsigns").setup()
-    end,
-}
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
 
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end)
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end)
+
+				map("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+				map("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+				map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", {})
+			end,
+		})
+	end,
+}

762099c112475be2b3c7600cee2d2122c2b1ea1c
Никита Ковалев <nikitakovalev@MacBook-Pro-nkovalev.local>
2025-04-02 12:53:15 +0300

    nvim: refactored lsp-config for easy install and config

--- nvim/.config/nvim/lua/just_kiy/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/just_kiy/plugins/lsp-config.lua
@@ -1,3 +1,14 @@
+local servers = {
+    "lua_ls",
+    "pylsp",
+    "gopls",
+    "bashls",
+    "docker_compose_language_service",
+    "dockerls",
+    "helm_ls",
+    "yamlls",
+}
+
 return {
     {
         "williamboman/mason.nvim",
@@ -9,7 +20,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ruff", "pylsp", "gopls" },
+                ensure_installed = servers,
             })
         end,
     },
@@ -19,41 +30,76 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
 
-            lspconfig.pylsp.setup({
+            -- Base config for all servers
+            local default_config = {
                 capabilities = capabilities,
-                settings = {
-                    pylsp = {
-                        plugins = {
-                            ruff = {
-                                enabled = true,
-                                formatEnabled = true,
-                            },
-                            rope = {
-                                enabled = true,
+            }
+
+            -- Server-specific overrides (deep merged)
+            local server_configs = {
+                pylsp = {
+                    settings = {
+                        pylsp = {
+                            plugins = {
+                                ruff = {
+                                    enabled = true,
+                                    formatEnabled = true,
+                                    formatOnSave = true,
+                                },
+                                jedi_completion = { enabled = false },
+                                jedi_rename = { enabled = false },
+                                rope_completion = { enabled = true },
+                                rope_autoimport = {
+                                    enabled = true,
+                                    completions = { enabled = true },
+                                    code_actions = { enabled = true },
+                                },
+                                pylsp_rope = {
+                                    enabled = true,
+                                    rename = true,
+                                },
+                                rope_rename = { enabled = false },
+                                pylsp_mypy = { enabled = false },
                             },
                         },
                     },
                 },
-            })
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.dartls.setup({
-                cmd = { "dart", "language-server", "--protocol=lsp" },
-            })
+            }
+
+            -- Auto-configure all servers
+            for _, server in ipairs(servers) do
+                local config = vim.tbl_deep_extend(
+                    "force",
+                    {},
+                    default_config,
+                    server_configs[server] or {}
+                )
+                lspconfig[server].setup(config)
+            end
+
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", {})
+            local telescope = require("telescope.builtin")
+            vim.keymap.set("n", "gd", telescope.lsp_definitions, {})
+            vim.keymap.set("n", "gr", telescope.lsp_references, {})
+            -- vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", {})
             vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", {})
             vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", {})
             vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", {})
-            vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", {})
+            -- vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", {})
             vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", {})
             vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+            vim.filetype.add({
+                filename = {
+                    ["docker-compose.yml"] = "yaml.docker-compose",
+                    ["docker-compose.yaml"] = "yaml.docker-compose",
+                    ["compose.yml"] = "yaml.docker-compose",
+                    ["compose.yaml"] = "yaml.docker-compose",
+                },
+            })
         end,
     },
 }

05d78cf36c0005036c6efe05a9fab3065296f604
Никита Ковалев <nikitakovalev@MacBook-Pro-nkovalev.local>
2025-07-16 12:57:58 +0300

    nvim: mini plugins

---
+++ nvim/.config/nvim/lua/just_kiy/plugins/mini.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        'echasnovski/mini.surround',
+        verion = false,
+        config = function()
+            require("mini.surround").setup()
+        end
+    },
+    {
+        'echasnovski/mini.pairs',
+        version = false,
+        config = function()
+            require("mini.pairs").setup()
+        end
+    }
+}
